69adb4357d3ca655c4ab8bc952d70b16342f93a0
Jimmy Ma <jiamingma1998@gmail.com>
2023-09-24 12:13:40 -0400

    up

--- discord/web.py
+++ discord/web.py
@@ -67,11 +67,11 @@ def handle_fam_level():
     body = request.json
     status, user = body["status"], body["user"]
     if status == "NotLeveledYet":
-        client_event(send("bot-spam-2", f"**[{dtFormat(datetime.now())} EST - Auto Fam Leveling]**\n{user}'s cousin determined that the familiars were not level 5 yet, trying again in 5 minutes", addToQueue=False))
+        client_event(send("bot-spam-2", f"**[{dtFormat(datetime.now())} EST - Auto Familiar Leveling]**\n{user}'s cousin determined that the familiars were not level 5 yet, trying again in 5 minutes", addToQueue=False))
     elif status == "Success":
-        client_event(send("bot-spam-2", f"**[{dtFormat(datetime.now())} EST - ToF]**\n{user}'s cousin finished leveling a set of familiars", addToQueue=False))
+        client_event(send("bot-spam-2", f"**[{dtFormat(datetime.now())} EST - Auto Familiar Leveling]**\n{user}'s cousin finished leveling a set of familiars", addToQueue=False))
     elif status == "Done":
-        client_event(send("bot-spam-2", f"**[{dtFormat(datetime.now())} EST - Auto Fam Leveling]**\n{user}'s cousin finished leveling all familiars, it will stop now", addToQueue=False))
+        client_event(send("bot-spam-2", f"**[{dtFormat(datetime.now())} EST - Auto Familiar Leveling]**\n{user}'s cousin finished leveling all familiars, it will stop now", addToQueue=False))
     return "Success", 200
 
 @app.route('/tof', methods=['POST'])
